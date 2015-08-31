f163265d7cf40e9f9c263f3dde4f47401f14cd10
Monsta <monsta@inbox.ru>
2015-08-31 17:57:50 +0300

    check for GTK+ release, not pre-release

--- applets/brightness/brightness-applet.c
+++ applets/brightness/brightness-applet.c
@@ -653,7 +653,7 @@ gpm_applet_create_popup (GpmBrightnessApplet *applet)
 
 	/* window */
 	applet->popup = gtk_window_new (GTK_WINDOW_POPUP);
-#if !GTK_CHECK_VERSION (2, 90, 0)
+#if !GTK_CHECK_VERSION (3, 0, 0)
 	GTK_WIDGET_UNSET_FLAGS (applet->popup, GTK_TOPLEVEL);
 #endif
 	gtk_window_set_type_hint (GTK_WINDOW(applet->popup), GDK_WINDOW_TYPE_HINT_UTILITY);
