1a034d225701ee422bfddcb3e979e0cab4f672e2
Zhang Xianwei <zhang.xianwei8@zte.com.cn>
2018-11-26 14:29:12 +0800

    media-keys: always round the render coordinates for media icons
    This ensures the pixbuf is always rendered as crisp as possible.
    
    See https://github.com/GNOME/gnome-settings-daemon/commit/a03c072a8241d4d481ee94e5e1ffd829e85271f5
    
    Signed-off-by: Zhang Xianwei <zhang.xianwei8@zte.com.cn>

--- src/gsd-media-keys-window.c
+++ src/gsd-media-keys-window.c
@@ -456,8 +456,8 @@ draw_action_volume (MsdMediaKeysWindow *window,
         volume_box_width = icon_box_width;
         volume_box_height = round (window_height * 0.05);
 
-        icon_box_x0 = (window_width - icon_box_width) / 2;
-        icon_box_y0 = (window_height - icon_box_height) / 2;
+        icon_box_x0 = round ((window_width - icon_box_width) / 2);
+        icon_box_y0 = round ((window_height - icon_box_height) / 2);
         volume_box_x0 = round (icon_box_x0);
         volume_box_y0 = round (window_height - icon_box_y0 / 2 - volume_box_height);
 
@@ -580,8 +580,8 @@ draw_action_custom (MsdMediaKeysWindow *window,
         bright_box_width = round (icon_box_width);
         bright_box_height = round (window_height * 0.05);
 
-        icon_box_x0 = (window_width - icon_box_width) / 2;
-        icon_box_y0 = (window_height - icon_box_height) / 2;
+        icon_box_x0 = round ((window_width - icon_box_width) / 2);
+        icon_box_y0 = round ((window_height - icon_box_height) / 2);
         bright_box_x0 = round (icon_box_x0);
         bright_box_y0 = round (window_height - icon_box_y0 / 2 - bright_box_height);
 
