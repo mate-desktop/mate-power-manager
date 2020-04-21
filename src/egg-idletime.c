ff2804623a22c2c58676354c78eac624290bbb53
Victor A. Santos <victoraur.santos@gmail.com>
2020-04-21 13:17:44 -0300

    Meson Project: dont change source files
    Revert changes to source files and meson will define HAVE_CONFIG_H.

--- src/egg-idletime.c
+++ src/egg-idletime.c
@@ -19,7 +19,9 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-#include <config.h>
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
 
 #include <glib.h>
 #include <X11/Xlib.h>
