7363e90aa4290e08cf86cf531206b23839a2b79d
raveit65 <mate@raveit.de>
2021-03-09 12:01:39 +0100

    gpm-prefs-core: fix building

--- src/gpm-prefs-core.c
+++ src/gpm-prefs-core.c
@@ -556,8 +556,9 @@ prefs_setup_battery (GpmPrefs *prefs)
 	g_settings_bind (prefs->priv->settings, GPM_SETTINGS_IDLE_DIM_BATT,
 			 widget, "active",
 			 G_SETTINGS_BIND_DEFAULT);
+	widget = GTK_WIDGET (gtk_builder_get_object (prefs->priv->builder, "checkbutton_battery_keyboard_reduce"));
 	g_settings_bind (prefs->priv->settings, GPM_SETTINGS_KBD_BACKLIGHT_BATT_REDUCE,
-			 GET_WIDGET ("checkbutton_battery_keyboard_reduce"), "active",
+			 widget, "active",
 			 G_SETTINGS_BIND_DEFAULT);
 
 	if (prefs->priv->has_button_lid == FALSE) {
