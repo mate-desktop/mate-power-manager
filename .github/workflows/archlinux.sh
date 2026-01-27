779eb45429aa22aec0aabfb03d6ab9f0a1185854
mbkma <johannes.unruh@fau.de>
2026-01-27 23:12:49 +0100

    ci: migrate from Travis CI to GitHub Actions
    Replace Travis CI configuration with GitHub Actions workflows.
    Add CI build workflow for Debian, Fedora, Ubuntu, and Archlinux.
    Add release workflow for automated GitHub releases on tags.
    Add dependabot configuration for GHA pin updates.
    Remove obsolete .travis.yml.

---
+++ .github/workflows/archlinux.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/bash
+
+set -eo pipefail
+
+# Use grouped output messages
+infobegin() {
+	echo "::group::${1}"
+}
+infoend() {
+	echo "::endgroup::"
+}
+
+# Required packages on Archlinux
+requires=(
+	ccache # Use ccache to speed up build
+	clang  # Build with clang on Archlinux
+	meson  # Used for meson build
+)
+
+# https://gitlab.archlinux.org/archlinux/packaging/packages/mate-power-manager
+requires+=(
+	autoconf-archive
+	dbus-glib
+	gcc
+	gettext
+	git
+	glib2-devel
+	gobject-introspection
+	itstool
+	libcanberra
+	libnotify
+	libsecret
+	make
+	mate-common
+	mate-panel
+	polkit
+	python
+	upower
+	which
+	yelp-tools
+)
+
+infobegin "Update system"
+pacman --noconfirm -Syu
+infoend
+
+infobegin "Install dependency packages"
+pacman --noconfirm -S ${requires[@]}
+infoend
---
+++ .github/workflows/fedora.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/bash
+
+set -eo pipefail
+
+# Use grouped output messages
+infobegin() {
+	echo "::group::${1}"
+}
+infoend() {
+	echo "::endgroup::"
+}
+
+# Required packages on Fedora
+requires=(
+	ccache # Use ccache to speed up build
+	meson  # Used for meson build
+)
+
+requires+=(
+	autoconf-archive
+	cairo-devel
+	dbus-glib-devel
+	desktop-file-utils
+	gcc
+	git
+	glib2-devel
+	gtk3-devel
+	libcanberra-devel
+	libgnome-keyring-devel
+	libnotify-devel
+	libsecret-devel
+	make
+	mate-common
+	mate-panel-devel
+	mesa-libGL-devel
+	polkit-devel
+	popt-devel
+	redhat-rpm-config
+	upower-devel
+	iso-codes-devel
+	gobject-introspection-devel
+	dconf-devel
+)
+
+infobegin "Update system"
+dnf update -y
+infoend
+
+infobegin "Install dependency packages"
+dnf install -y ${requires[@]}
+infoend
