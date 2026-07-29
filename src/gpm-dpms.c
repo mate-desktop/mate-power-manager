34908ab026d334fb5490d740742f88b702e85650
Aleksey Samoilov <samoilov.lex@gmail.com>
2026-07-29 11:36:22 +0400

    Implement wlr-output-power-management

--- src/gpm-dpms.c
+++ src/gpm-dpms.c
@@ -45,6 +45,8 @@
 
 #ifdef HAVE_WAYLAND
 #include <gdk/gdkwayland.h>
+#include <wayland-client.h>
+#include "wlr-output-power-management-unstable-v1-client.h"
 #endif /* HAVE_WAYLAND */
 
 #include "gpm-dpms.h"
@@ -54,6 +56,14 @@ static void   gpm_dpms_finalize  (GObject   *object);
 /* until we get a nice event-emitting DPMS extension, we have to poll... */
 #define GPM_DPMS_POLL_TIME	10
 
+#ifdef HAVE_WAYLAND
+typedef struct {
+	struct zwlr_output_power_v1 *power;
+	uint32_t name;
+	GpmDpmsMode mode;
+} GpmDpmsWaylandOutput;
+#endif
+
 struct GpmDpmsPrivate
 {
 	gboolean		 dpms_capable;
@@ -61,6 +71,10 @@ struct GpmDpmsPrivate
 	guint			 timer_id;
 #ifdef HAVE_X11
 	Display			*display;
+#endif
+#ifdef HAVE_WAYLAND
+	struct zwlr_output_power_manager_v1 *power_manager;
+	GPtrArray *wayland_outputs;
 #endif
 };
 
@@ -220,6 +234,148 @@ out:
 
 #endif /* HAVE_X11 */
 
+#ifdef HAVE_WAYLAND
+
+static void
+gpm_dpms_wayland_output_mode_cb (void *data,
+				 struct zwlr_output_power_v1 *power,
+				 uint32_t mode)
+{
+	GpmDpmsWaylandOutput *output = data;
+	GpmDpmsMode new_mode;
+
+	switch (mode) {
+	case ZWLR_OUTPUT_POWER_V1_MODE_OFF:
+		new_mode = GPM_DPMS_MODE_OFF;
+		break;
+	case ZWLR_OUTPUT_POWER_V1_MODE_ON:
+	default:
+		new_mode = GPM_DPMS_MODE_ON;
+		break;
+	}
+
+	if (new_mode != output->mode) {
+		output->mode = new_mode;
+	}
+}
+
+static void
+gpm_dpms_wayland_output_failed_cb (void *data,
+				   struct zwlr_output_power_v1 *power)
+{
+	GpmDpmsWaylandOutput *output = data;
+	g_debug ("wlr-output-power failed for output %u", output->name);
+}
+
+static const struct zwlr_output_power_v1_listener
+gpm_dpms_wayland_power_listener = {
+	.mode = gpm_dpms_wayland_output_mode_cb,
+	.failed = gpm_dpms_wayland_output_failed_cb,
+};
+
+static GpmDpmsWaylandOutput *
+gpm_dpms_wayland_output_find (GpmDpms *dpms, uint32_t name)
+{
+	guint i;
+	for (i = 0; i < dpms->priv->wayland_outputs->len; i++) {
+		GpmDpmsWaylandOutput *o = g_ptr_array_index (dpms->priv->wayland_outputs, i);
+		if (o->name == name)
+			return o;
+	}
+	return NULL;
+}
+
+static void
+gpm_dpms_wayland_registry_global_cb (void *data, struct wl_registry *registry,
+				     uint32_t name, const char *interface,
+				     uint32_t version)
+{
+	GpmDpms *dpms = data;
+
+	if (g_strcmp0 (interface, "zwlr_output_power_manager_v1") == 0) {
+		dpms->priv->power_manager = wl_registry_bind (registry, name,
+			&zwlr_output_power_manager_v1_interface, 1);
+		g_debug ("bound zwlr_output_power_manager_v1");
+	} else if (g_strcmp0 (interface, "wl_output") == 0) {
+		struct wl_output *output;
+		GpmDpmsWaylandOutput *wo;
+
+		wo = gpm_dpms_wayland_output_find (dpms, name);
+		if (wo != NULL)
+			return;
+
+		output = wl_registry_bind (registry, name,
+					   &wl_output_interface, 1);
+		if (dpms->priv->power_manager == NULL)
+			return;
+
+		wo = g_new0 (GpmDpmsWaylandOutput, 1);
+		wo->name = name;
+		wo->mode = GPM_DPMS_MODE_ON;
+		wo->power = zwlr_output_power_manager_v1_get_output_power (
+			dpms->priv->power_manager, output);
+		zwlr_output_power_v1_add_listener (wo->power,
+			&gpm_dpms_wayland_power_listener, wo);
+		g_ptr_array_add (dpms->priv->wayland_outputs, wo);
+		g_debug ("added zwlr_output_power_v1 for output %u", name);
+	}
+}
+
+static void
+gpm_dpms_wayland_registry_global_remove_cb (void *data,
+					    struct wl_registry *registry,
+					    uint32_t name)
+{
+	GpmDpms *dpms = data;
+	GpmDpmsWaylandOutput *wo = gpm_dpms_wayland_output_find (dpms, name);
+	if (wo == NULL)
+		return;
+	if (wo->power)
+		zwlr_output_power_v1_destroy (wo->power);
+	g_ptr_array_remove (dpms->priv->wayland_outputs, wo);
+	g_free (wo);
+	g_debug ("removed zwlr_output_power_v1 for output %u", name);
+}
+
+static const struct wl_registry_listener gpm_dpms_wayland_registry_listener = {
+	.global = gpm_dpms_wayland_registry_global_cb,
+	.global_remove = gpm_dpms_wayland_registry_global_remove_cb,
+};
+
+static gboolean
+gpm_dpms_wayland_set_mode (GpmDpms *dpms, GpmDpmsMode mode, GError **error)
+{
+	guint i;
+	uint32_t wlr_mode;
+
+	if (dpms->priv->wayland_outputs->len == 0) {
+		g_set_error (error, GPM_DPMS_ERROR, GPM_DPMS_ERROR_GENERAL,
+			     "No Wayland outputs available");
+		return FALSE;
+	}
+
+	switch (mode) {
+	case GPM_DPMS_MODE_OFF:
+		wlr_mode = ZWLR_OUTPUT_POWER_V1_MODE_OFF;
+		break;
+	case GPM_DPMS_MODE_ON:
+	case GPM_DPMS_MODE_STANDBY:
+	case GPM_DPMS_MODE_SUSPEND:
+	default:
+		wlr_mode = ZWLR_OUTPUT_POWER_V1_MODE_ON;
+		break;
+	}
+
+	for (i = 0; i < dpms->priv->wayland_outputs->len; i++) {
+		GpmDpmsWaylandOutput *wo = g_ptr_array_index (dpms->priv->wayland_outputs, i);
+		zwlr_output_power_v1_set_mode (wo->power, wlr_mode);
+	}
+
+	return TRUE;
+}
+
+#endif /* HAVE_WAYLAND */
+
 /**
  * gpm_dpms_set_mode:
  **/
@@ -241,6 +397,12 @@ gpm_dpms_set_mode (GpmDpms *dpms, GpmDpmsMode mode, GError **error)
 		return gpm_dpms_x11_set_mode (dpms, mode, error);
 	}
 #endif
+#ifdef HAVE_WAYLAND
+	if (gdk_display_get_default () != NULL &&
+	    GDK_IS_WAYLAND_DISPLAY (gdk_display_get_default ())) {
+		return gpm_dpms_wayland_set_mode (dpms, mode, error);
+	}
+#endif
 	g_set_error (error, GPM_DPMS_ERROR, GPM_DPMS_ERROR_GENERAL,
 		     "DPMS not supported on this display");
 	return FALSE;
@@ -258,6 +420,27 @@ gpm_dpms_get_mode (GpmDpms *dpms, GpmDpmsMode *mode, GError **error)
 	if (gdk_display_get_default () != NULL &&
 	    GDK_IS_X11_DISPLAY (gdk_display_get_default ())) {
 		return gpm_dpms_x11_get_mode (dpms, mode, error);
+	}
+#endif
+#ifdef HAVE_WAYLAND
+	if (gdk_display_get_default () != NULL &&
+	    GDK_IS_WAYLAND_DISPLAY (gdk_display_get_default ())) {
+		GpmDpmsMode result = GPM_DPMS_MODE_ON;
+		guint i;
+		gboolean all_off = TRUE;
+
+		for (i = 0; i < dpms->priv->wayland_outputs->len; i++) {
+			GpmDpmsWaylandOutput *wo = g_ptr_array_index (dpms->priv->wayland_outputs, i);
+			if (wo->mode != GPM_DPMS_MODE_OFF) {
+				all_off = FALSE;
+				break;
+			}
+		}
+		if (all_off && dpms->priv->wayland_outputs->len > 0)
+			result = GPM_DPMS_MODE_OFF;
+		if (mode)
+			*mode = result;
+		return TRUE;
 	}
 #endif
 	if (mode)
@@ -341,7 +524,34 @@ gpm_dpms_init (GpmDpms *dpms)
 #ifdef HAVE_WAYLAND
 	if (gdk_display_get_default () != NULL &&
 	    GDK_IS_WAYLAND_DISPLAY (gdk_display_get_default ())) {
-		g_debug ("Wayland DPMS not yet implemented");
+		struct wl_display *display;
+		struct wl_registry *registry;
+
+		dpms->priv->wayland_outputs = g_ptr_array_new_with_free_func (g_free);
+
+		display = gdk_wayland_display_get_wl_display (gdk_display_get_default ());
+		if (display == NULL) {
+			g_warning ("Cannot get Wayland display");
+			return;
+		}
+
+		registry = wl_display_get_registry (display);
+		if (registry == NULL) {
+			g_warning ("Cannot get Wayland registry");
+			return;
+		}
+
+		wl_registry_add_listener (registry,
+			&gpm_dpms_wayland_registry_listener, dpms);
+		wl_display_roundtrip (display);
+
+		if (dpms->priv->power_manager == NULL) {
+			g_debug ("Compositor does not support zwlr_output_power_manager_v1");
+		}
+
+		dpms->priv->timer_id = g_timeout_add_seconds (GPM_DPMS_POLL_TIME,
+			(GSourceFunc) gpm_dpms_poll_mode_cb, dpms);
+		g_source_set_name_by_id (dpms->priv->timer_id, "[GpmDpms] poll");
 		return;
 	}
 #endif
@@ -367,6 +577,21 @@ gpm_dpms_finalize (GObject *object)
 		g_source_remove (dpms->priv->timer_id);
 		dpms->priv->timer_id = 0;
 	}
+
+#ifdef HAVE_WAYLAND
+	if (gdk_display_get_default () != NULL &&
+	    GDK_IS_WAYLAND_DISPLAY (gdk_display_get_default ())) {
+		guint i;
+		for (i = 0; i < dpms->priv->wayland_outputs->len; i++) {
+			GpmDpmsWaylandOutput *wo = g_ptr_array_index (dpms->priv->wayland_outputs, i);
+			if (wo->power)
+				zwlr_output_power_v1_destroy (wo->power);
+		}
+		g_ptr_array_free (dpms->priv->wayland_outputs, TRUE);
+		if (dpms->priv->power_manager)
+			zwlr_output_power_manager_v1_destroy (dpms->priv->power_manager);
+	}
+#endif
 
 	G_OBJECT_CLASS (gpm_dpms_parent_class)->finalize (object);
 }
